8351d676dbab1520bee4395f37276247cd46b43b
gs-msreevani <msreevani@gainsight.com>
2025-10-17 13:06:22 +0530

    adding css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import TaskColumn from "./TaskColumn";
 import todoIcon from "../assets/direct-hit.png";
 import doingIcon from "../assets/glowing-star.png";
 import doneIcon from "../assets/check-mark-button.png";
+import "./Home.css";
 
 const Home = () => {
   useEffect(() => {
     document.title = "Homepage"; // Set the page title
   }, []);
   return (
-    <div>
-      <h1>I am home page</h1>
+    <div className="app">
       <TaskForm />
       <header className="app_header">Header Section</header>
       <main className="app_main">
