41824706f4c890c0bc7e78a07f5678432bf1a1f4
gs-msreevani <msreevani@gainsight.com>
2025-10-28 18:34:17 +0530

    adding custom events

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,22 @@ import React, { useEffect } from "react";
 const About = () => {
   useEffect(() => {
     document.title = "About Page"; // Set the page title
+    // Function to run on DOMContentLoaded
+    const fullUrl = window.location.href;
+    const currentPageType = fullUrl.includes("about") ? "about" : "other";
+    console.log(currentPageType);
   }, []);
-  return <div id="about">I am about page</div>;
+  return (
+    <div id="about">
+      <p>I am about page</p>
+      <a
+        href="https://en.wikipedia.org/wiki/ChatGPT"
+        target="_blank"
+        rel="noopener noreferrer">
+        ChatGPT
+      </a>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,26 @@ import React, { useEffect } from "react";
 const Contact = () => {
   useEffect(() => {
     document.title = "Contact Page"; // Set the page title
+    const fullUrl = window.location.href;
+    const currentPageType = fullUrl.includes("contact") ? "contact" : "other";
+    console.log(currentPageType);
+    if (typeof window.aptrinsic === "function") {
+      window.aptrinsic("set", "user", { pageType: currentPageType });
+      console.log(
+        "Aptrinsic user attribute set with pageType:",
+        currentPageType
+      );
+    }
   }, []);
   return (
     <div>
       <h1> I am contact page </h1>
+      <a
+        href="https://en.wikipedia.org/wiki/Millet"
+        target="_blank"
+        rel="noopener noreferrer">
+        Millets
+      </a>
     </div>
   );
 };
